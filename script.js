acb5cf055d5df876ced030126ac7a149e92fea6d
Andrew Bautista <andrewbautista85@gmail.com>
2020-01-17 15:03:08 -0500

    adjusted createElement function for only four arguments, removed name variable for tmp

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var contentId = document.getElementById("content");
 var countdown = 0;
 
 // This function will handle the basic creation of an element, one attribute, and text content
-function createElement(a, b, c, d, e) {
-    var a = document.createElement(b);
-    a.setAttribute(c, d);
-    a.textContent = e;
-    return a;
+function createElement(a, b, c, d) {
+    var tmp = document.createElement(a);
+    tmp.setAttribute(b, c);
+    tmp.textContent = d;
+    return tmp;
 };
 
 // This function will be used to append the Child.
@@ -19,23 +19,23 @@ function appendChild(x, y) {
 };
 
 // Creating View Highscore
-var highScoreDiv = createElement(highScoreDiv, "div", "id", "high-scores", "View Highscores");
+var highScoreDiv = createElement("div", "id", "high-scores", "View Highscores");
 appendChild(headerEl, highScoreDiv);
 
 //Creating Timer
-var timerDiv = createElement(timerDiv, "div", "id", "timer", "Timer: " + countdown);
+var timerDiv = createElement("div", "id", "timer", "Timer: " + countdown);
 appendChild(headerEl, timerDiv);
 
 //Creating h1 for header/questions
-var questionH1 = createElement(questionH1, "h1", "id", "heading", "Coding Quiz Challenge");
+var questionH1 = createElement("h1", "id", "heading", "Coding Quiz Challenge");
 appendChild(contentId, questionH1);
 
 //Creating Description of Quiz
-var descriptionDiv = createElement(descriptionDiv, "p", "id", "description", "Try to answer the following code - related questions within the time limit. Keep in mind that incorrect answers will penalize your scoretime by ten seconds!");
+var descriptionDiv = createElement("p", "id", "description", "Try to answer the following code - related questions within the time limit. Keep in mind that incorrect answers will penalize your scoretime by ten seconds!");
 appendChild(contentId, descriptionDiv);
 
 //Creating Button to Start Quiz, needing to add one more Attribute to this one
-var startButton = createElement(startButton, "button", "id", "start-quiz", "Start Quiz");
+var startButton = createElement("button", "id", "start-quiz", "Start Quiz");
 startButton.setAttribute("type", "button");
 appendChild(contentId, startButton);
 
